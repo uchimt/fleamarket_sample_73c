d4bf7beedf369213cd093cbe5996ac3b532d74e3
masayakaneko <kane.coma@gmail.com>
2020-07-04 03:08:07 +0900

    ウィザード形式
    テーブル名カラム名意外カリキュラム通りに進めているのでまとめています
    
    curriculums/3994

---
+++ app/models/profile.rb
@@ -0,0 +1,4 @@
+class Profile < ApplicationRecord
+  belongs_to :user, optional: true
+  validates :first_name, :family_name, :first_name_kana, :family_name_kana, :birth_year, :birth_month, :birth_day ,presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_02_122528) do
+ActiveRecord::Schema.define(version: 2020_07_03_163423) do
+
+  create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "family_name", null: false
+    t.string "first_name_kana", null: false
+    t.string "family_name_kana", null: false
+    t.integer "birth_year", null: false
+    t.integer "birth_month", null: false
+    t.integer "birth_day", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -25,4 +39,5 @@ ActiveRecord::Schema.define(version: 2020_07_02_122528) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "profiles", "users"
 end
