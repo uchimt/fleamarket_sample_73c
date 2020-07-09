674a7d060e1b3a15505a84e4f2f6b05e42e26603
masayakaneko <kane.coma@gmail.com>
2020-07-09 22:06:21 +0900

    郵便番号
    0から始まる地域もあるため

--- db/migrate/20200703181357_create_destinations.rb
+++ db/migrate/20200703181357_create_destinations.rb
@@ -7,7 +7,7 @@ class CreateDestinations < ActiveRecord::Migration[5.2]
       t.string :destination_family_name_kana,  null:false
       t.string :destination_first_name_kana,   null:false
 
-      t.integer :postal_code,                  null:false
+      t.string :postal_code,                   null:false
       t.string :prefecture,                    null:false
       t.string :city,                          null:false
       t.string :address,                       null:false
