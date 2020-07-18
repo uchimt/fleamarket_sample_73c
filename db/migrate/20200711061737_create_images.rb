fce9a88fefb7db714be1dba085cf737065947b7c
yuhikimura <yuhi.kimura.0720@gmail.com>
2020-07-18 14:07:38 +0900

    update

--- db/migrate/20200711061737_create_images.rb
+++
@@ -1,9 +0,0 @@
-class CreateImages < ActiveRecord::Migration[5.2]
-  def change
-    create_table :images do |t|
-      t.string :url
-      t.references :product
-      t.timestamps
-    end
-  end
-end
