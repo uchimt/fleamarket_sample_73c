55c61efb0f15e6311702b0b174e7dd7ac13814ec
ayano <ichijikukobachi@gmail.com>
2020-07-23 16:12:58 +0900

    remove_add_references_prpducts

--- db/migrate/20200719152035_add_foreign_keyd_brand_id_to_products.rb
+++ db/migrate/20200719152035_add_foreign_keyd_brand_id_to_products.rb
@@ -1,7 +1,6 @@
 class AddForeignKeydBrandIdToProducts < ActiveRecord::Migration[5.2]
   def change
     add_reference :products, :brand, foreign_key: true
-    add_reference :products, :
-    add_reference :products,
+    
   end
 end
