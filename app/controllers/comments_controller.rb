13276556bf5d10f049f4139b961d36cfca135cc8
kawayu-yu <yuya.2a.0758@gmail.com>
2020-08-04 18:15:35 +0900

    ストロングパラメータを定義

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,4 +3,9 @@ class CommentsController < ApplicationController
     comment = Comment.create(comment_params)
     redirect_to product_path                  # 商品詳細画面へ遷移
   end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:comment).merage(user_id: current_user.id)
+  end
 end
