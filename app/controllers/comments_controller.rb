27a459615407ead86b68015c2dc91202cd2f20a2
kawayu-yu <yuya.2a.0758@gmail.com>
2020-08-07 21:33:10 +0900

    product_idを読み込めるように修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,11 @@
 class CommentsController < ApplicationController
   def create
-    comment = Comment.create(comment_params)
-    redirect_to product_path                  # 商品詳細画面へ遷移
+    @comment = Comment.create(comment_params)
+    redirect_to product_path(@comment.product.id)  # 商品詳細画面へ遷移
   end
 
   private
   def comment_params
-    params.require(:comment).permit(:comment).merage(user_id: current_user.id)
+    params.require(:comment).permit(:comment).merge(user_id: current_user.id, product_id: params[:product_id])
   end
 end
