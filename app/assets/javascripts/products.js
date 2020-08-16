f34d6633a966f0ffd393bbc50542e477a71eb595
uchimt <dosanco.ucchi@gmail.com>
2020-08-17 00:29:11 +0900

    画像編集用にfileIndex追加

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -65,7 +65,7 @@ $(function() {
   //
 
   // file_fieldのnameに動的なindexをつける為の配列
-  let fileIndex = [1,2,3,4,5];
+  let fileIndex = [1,2,3,4,5,6,7,8,9,10];
   // 既に使われているindexを除外
   lastIndex = $('.js-file_group:last').data('index');
   fileIndex.splice(0, lastIndex);
