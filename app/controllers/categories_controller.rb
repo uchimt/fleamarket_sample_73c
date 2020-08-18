cbe2d089ddb1f5adb51359c3eb8eceb9acde1acc
uchimt <dosanco.ucchi@gmail.com>
2020-08-18 19:44:42 +0900

    カテゴリーで商品一覧を表示

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,12 @@
 class CategoriesController < ApplicationController
     
   def index
+    @parents = Category.where(ancestry: nil).order('id ASC').limit(13) #１層目が１３個なのでlimit(13)
   end
+
+  def show
+    @category = Category.find(params[:id])
+    @category_products = Product.includes(:images).where(category_id: params[:id]).order('created_at DESC')
+  end
+
 end
