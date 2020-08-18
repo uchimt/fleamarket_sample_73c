806399ade00051dbd513bf3aeda3ebd4f55a3087
uchimt <dosanco.ucchi@gmail.com>
2020-08-19 04:07:03 +0900

    カテゴリー商品一覧マークアップ

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,17 +6,12 @@ class CategoriesController < ApplicationController
   end
 
   def show
-    @category_products =@category.set_products
-    @category_products = @category_products.includes(:images).order('created_at DESC').page(params[:page]).per(25)
+    @category_products = @category.set_products
+    @category_products = @category_products.includes(:images).order('created_at DESC').page(params[:page]).per(20)
   end
 
   private
   def set_category
     @category = Category.find(params[:id])
-    if @category.has_children?
-      @category_links = @category.children
-    else
-      @category_links = @category.siblings
-    end
   end
 end
