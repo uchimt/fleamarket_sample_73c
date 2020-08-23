eea1c72c4004d52abfa88cdf7ec6a37e0103c012
ayano <ichijikukobachi@gmail.com>
2020-08-23 10:35:03 +0900

    newアクションのテストコード

--- spec/factories/credit_cards.rb
+++ spec/factories/credit_cards.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   
   factory :CreditCard do
-    user_id
-    card_id
-    customer_id  {cus_}
+    user_id      {"1"}
+    card_id      {car_0b8a6ea1a13401ef98489dc4a69f}
+    customer_id  {cus_58967219b45ff3a0d1c05f1772e4}
     
   end
 end
